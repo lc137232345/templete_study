e46750b2a545834585b9b751abd4bc89efe8b934
lichen <137232345@qq.com>
2020-10-28 22:57:11 -0700

    测试

---
+++ project-1/study1.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-28 19:09:27
+ * @LastEditTime: 2020-10-28 19:46:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /code/templete_study/project-1/study1.cpp
+ */
+#include <iostream>
+#include <string>
+#include "study1.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    try
+    {
+        Stack<int> intStack;
+        Stack<string> stringStack;
+        intStack.push(7);
+        std::cout << intStack.top() << std::endl;
+        stringStack.push("hell world");
+        std::cout << stringStack.top() << std::endl;
+        stringStack.pop();
+        stringStack.pop();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "exception: "<<e.what() << '\n';
+    }
+    
+    std::cout << "/* message */" << std::endl;
+    return 0;
+}
---
+++ project-1/study1.h
@@ -0,0 +1,47 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-28 19:21:13
+ * @LastEditTime: 2020-10-28 19:44:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /templete_study/project-1/study1.h
+ */
+#include <vector>
+#include <stdexcept>
+template <typename T>
+class Stack{  
+    private:
+        std::vector<T> elems;
+    public:
+        
+        void push(T const & value);
+        T pop();
+        T top() const;
+
+};
+template <typename T>
+void Stack<T>::push(T const & value)
+{
+    elems.push_back(value);
+};
+
+template <typename T>
+T Stack<T>::pop()
+{
+    if(elems.empty())
+    {
+        throw std::out_of_range("err");
+    }
+    T elem=elems.back();
+    elems.pop_back();
+    return elem;
+};
+
+template <typename T>
+T Stack<T>::top() const{
+    if(elems.empty())
+    {
+        throw std::out_of_range("err");
+    }
+    return elems.back();
+};
